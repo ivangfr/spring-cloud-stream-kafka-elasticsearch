5bdb18dc1be2c31c7ab5a06c7c69f67c804a2c97
Ivan Franchin <ivan.franchin@takeaway.com>
2018-09-11 22:44:17 +0200

    add eureka-server

--- categorizer-service/src/main/java/com/mycompany/categorizerservice/CategorizerServiceApplication.java
+++ categorizer-service/src/main/java/com/mycompany/categorizerservice/CategorizerServiceApplication.java
@@ -2,11 +2,13 @@ package com.mycompany.categorizerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CategorizerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CategorizerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CategorizerServiceApplication.class, args);
+    }
 }
--- publisher-api/src/main/java/com/mycompany/publisherapi/PublisherApiApplication.java
+++ publisher-api/src/main/java/com/mycompany/publisherapi/PublisherApiApplication.java
@@ -2,11 +2,13 @@ package com.mycompany.publisherapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PublisherApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PublisherApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherApiApplication.class, args);
+    }
 }
