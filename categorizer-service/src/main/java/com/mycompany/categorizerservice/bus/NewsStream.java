56c3292345f50ee82a8e0613e0f0acaaf40dda1c
Ivan Franchin <ivan.franchin@takeaway.com>
2019-01-08 17:07:14 -0200

    changes: - update spring-boot version to 2.0.7; - using Payload annotation to get the payload from a message; - add dependency spring-cloud-stream-test-support to parent pom; - remove dependency org.springframework.kafka:spring-kafka; - add new services to docker-compose: kafka-rest-proxy and kafka-topics-ui; - update README.

--- categorizer-service/src/main/java/com/mycompany/categorizerservice/bus/NewsStream.java
+++ categorizer-service/src/main/java/com/mycompany/categorizerservice/bus/NewsStream.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.IntegrationMessageHeaderAccessor;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
@@ -26,12 +26,11 @@ public class NewsStream {
 
     @StreamListener(Processor.INPUT)
     @SendTo(Processor.OUTPUT)
-    public NewsEvent handleNewsEvent(Message<NewsEvent> message,
+    public NewsEvent handleNewsEvent(@Payload NewsEvent newsEvent,
                                      @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                                     @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-                                     @Header(KafkaHeaders.OFFSET) long offset,
-                                     @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) int deliveryAttempt) {
-        NewsEvent newsEvent = message.getPayload();
+                                     @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+                                     @Header(KafkaHeaders.OFFSET) Long offset,
+                                     @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) Integer deliveryAttempt) {
         log.info("NewsEvent with id '{}' and title '{}' received from bus. topic: {}, partition: {}, offset: {}, deliveryAttempt: {}",
                 newsEvent.getId(), newsEvent.getTitle(), topic, partition, offset, deliveryAttempt);
 
--- news-client/src/main/java/com/mycompany/newsclient/bus/NewsStream.java
+++ news-client/src/main/java/com/mycompany/newsclient/bus/NewsStream.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.integration.IntegrationMessageHeaderAccessor;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -26,12 +26,11 @@ public class NewsStream {
     }
 
     @StreamListener(Sink.INPUT)
-    public void handleNewsEvent(Message<NewsEvent> message,
+    public void handleNewsEvent(@Payload NewsEvent newsEvent,
                                 @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-                                @Header(KafkaHeaders.OFFSET) long offset,
-                                @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) int deliveryAttempt) {
-        NewsEvent newsEvent = message.getPayload();
+                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+                                @Header(KafkaHeaders.OFFSET) Long offset,
+                                @Header(IntegrationMessageHeaderAccessor.DELIVERY_ATTEMPT) Integer deliveryAttempt) {
         log.info("NewsEvent with id '{}' and title '{}' received from bus. topic: {}, partition: {}, offset: {}, deliveryAttempt: {}",
                 newsEvent.getId(), newsEvent.getTitle(), topic, partition, offset, deliveryAttempt);
 
