1ad81e152be04051ba7ad1f8c0221a96452bf377
Ivan Franchin <ivan.franchin@takeaway.com>
2019-04-11 23:37:29 +0200

    update spring-boot version to 2.1.4

--- collector-service/src/main/java/com/mycompany/collectorservice/config/ElasticsearchConfig.java
+++ collector-service/src/main/java/com/mycompany/collectorservice/config/ElasticsearchConfig.java
@@ -3,13 +3,11 @@ package com.mycompany.collectorservice.config;
 import com.mycompany.collectorservice.properties.CollectorServiceProperties;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -26,7 +24,7 @@ public class ElasticsearchConfig {
 
     @Bean
     Client client() throws UnknownHostException {
-        InetSocketTransportAddress transportAddress = new InetSocketTransportAddress(
+        TransportAddress transportAddress = new TransportAddress(
                 InetAddress.getByName(collectorServiceProperties.getElasticsearch().getHost()),
                 collectorServiceProperties.getElasticsearch().getPort());
 
@@ -34,11 +32,6 @@ public class ElasticsearchConfig {
                 .put("cluster.name", collectorServiceProperties.getElasticsearch().getClustername()).build();
 
         return new PreBuiltTransportClient(settings).addTransportAddress(transportAddress);
-    }
-
-    @Bean
-    ElasticsearchOperations elasticsearchTemplate() throws UnknownHostException {
-        return new ElasticsearchTemplate(client());
     }
 
 }
--- publisher-api/src/main/java/com/mycompany/publisherapi/config/ElasticsearchConfig.java
+++ publisher-api/src/main/java/com/mycompany/publisherapi/config/ElasticsearchConfig.java
@@ -3,13 +3,11 @@ package com.mycompany.publisherapi.config;
 import com.mycompany.publisherapi.properties.PublisherApiProperties;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
 import java.net.InetAddress;
 
@@ -25,7 +23,7 @@ public class ElasticsearchConfig {
 
     @Bean
     Client client() throws Exception {
-        InetSocketTransportAddress transportAddress = new InetSocketTransportAddress(
+        TransportAddress transportAddress = new TransportAddress(
                 InetAddress.getByName(publisherApiProperties.getElasticsearch().getHost()),
                 publisherApiProperties.getElasticsearch().getPort());
 
@@ -33,11 +31,6 @@ public class ElasticsearchConfig {
                 .put("cluster.name", publisherApiProperties.getElasticsearch().getClustername()).build();
 
         return new PreBuiltTransportClient(settings).addTransportAddress(transportAddress);
-    }
-
-    @Bean
-    ElasticsearchOperations elasticsearchTemplate() throws Exception {
-        return new ElasticsearchTemplate(client());
     }
 
 }
