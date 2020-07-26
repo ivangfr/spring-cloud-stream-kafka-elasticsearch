336c8e51b91a01a55ad9c4cf08306746d0d7f06c
Ivan Franchin <ivan.franchin@takeaway.com>
2020-07-26 09:46:47 +0200

    implement a new ui to news-client using semantic-ui framework

--- news-client/src/main/resources/static/app.js
+++ news-client/src/main/resources/static/app.js
@@ -1,49 +1,56 @@
-var stompClient = null;
+let stompClient = null
 
 function connect() {
-    var socket = new SockJS('/news-websocket');
-    stompClient = Stomp.over(socket);
+    const socket = new SockJS('/news-websocket')
+    stompClient = Stomp.over(socket)
+
+    const $webSocketSwitch = $('#webSocketSwitch')
 
     stompClient.connect({},
         function (frame) {
-            console.log('Connected: ' + frame);
+            console.log('Connected: ' + frame)
             stompClient.subscribe('/topic/news', function (news) {
-                var newsBody = JSON.parse(news.body);
-                var card = '<div class="card">' +
-                               '<div class="card-content">' +
-                                   '<span class="left blue lighten-4">'+newsBody.category.toUpperCase()+'</span>' +
-                                   '<span class="right grey-text lighten-4">'+moment(newsBody.datetime).format("DD-MMM-YYYY HH:mm:ss")+'</span>' +
-                                   '<div class="clearfix"></div>' +
-                                   '<h5>'+newsBody.title+'</h5>' +
-                                   '<div class="divider"></div>' +
-                                   '<p>'+newsBody.text+'</p>' +
-                               '</div>' +
-                           '</div>';
-                $('#newsList').prepend(card);
-            });
+                const newsBody = JSON.parse(news.body)
+                const newsItem = '<div class="item">' +
+                                   '<div class="image">' +
+                                     '<img src="img/'+newsBody.category+'.jpg" style="width:150px; height:120px">' +
+                                   '</div>' +
+                                   '<div class="content">' +
+                                     '<div class="meta" style="float:right">' +
+                                       '<span>'+moment(newsBody.datetime).format("DD-MMM-YYYY HH:mm:ss")+'</span>' +
+                                     '</div>' +
+                                     '<div class="ui medium blue header">'+newsBody.category.toUpperCase()+'</div>' +
+                                     '<div class="ui divider"></div>' +
+                                     '<div class="ui large header">'+newsBody.title+'</div>' +
+                                     '<div class="description">' +
+                                       '<p>'+newsBody.text+'</p>' +
+                                     '</div>' +
+                                   '</div>' +
+                                 '</div>'
+
+                $('#newsList').prepend(newsItem)
+            })
+            $webSocketSwitch.removeClass('disabled')
         },
         function() {
-            console.log('Unable to connect to Websocket!');
-            $('#websocketSwitch').prop('checked', false);
+            console.log('Unable to connect to Websocket!')
+            $webSocketSwitch.addClass('disabled')
         }
-     );
+     )
 }
 
 function disconnect() {
     if (stompClient !== null) {
-        stompClient.disconnect();
+        stompClient.disconnect()
     }
-    console.log("Disconnected");
+    console.log("Disconnected")
 }
 
 $(function () {
-    $('#websocketSwitch').click(function() {
-        if ($(this).prop('checked')) {
-            connect();
-        } else {
-            disconnect();
-        }
-    });
-});
+    $('#webSocketSwitch').checkbox({
+        onChecked: function() { connect() },
+        onUnchecked: function() { disconnect() }
+    })
+})
 
-connect();
+connect()
