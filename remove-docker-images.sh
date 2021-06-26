2cfe69b9ee58788732de9a5edd0432338324f820
Ivan Franchin <ivan.franchin@takeaway.com>
2021-06-26 18:00:59 +0200

    project update - update to spring-boot 2.5.2; - update to jib 3.1.1; - disable swagger petstore sample; - add remove-docker-images.sh; - update README.

---
+++ remove-docker-images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker rmi ivanfranchin/eureka-server:1.0.0
+docker rmi ivanfranchin/producer-api:1.0.0
+docker rmi ivanfranchin/categorizer-service:1.0.0
+docker rmi ivanfranchin/collector-service:1.0.0
+docker rmi ivanfranchin/publisher-api:1.0.0
+docker rmi ivanfranchin/news-client:1.0.0
