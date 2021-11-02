22be810d0dcf7d274dc4122d4bd831745b9f818b
Ivan Franchin <ivan.franchin@takeaway.com>
2021-11-02 18:16:07 +0100

    project update - update to spring-boot 2.5.6; - update to springdoc-openapi 1.5.12; - update to openjdk docker image 11.0.13; - update to maven 3.8.3; - update to zipkin docker image 2.23.4; - rename images folder to documentation; - remove start_period from docker-compose.yml; - remove health-start-period from start-apps.sh; - update README.

--- start-apps.sh
+++ start-apps.sh
@@ -7,7 +7,7 @@ echo "Starting eureka..."
 
 docker run -d --rm --name eureka -p 8761:8761 \
   --network spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8761/actuator/health || exit 1" --health-start-period=30s \
+  --health-cmd="curl -f http://localhost:8761/actuator/health || exit 1" \
   ivanfranchin/eureka-server:1.0.0
 
 wait_for_container_log "eureka" "Started"
@@ -18,7 +18,7 @@ echo "Starting producer-api..."
 docker run -d --rm --name producer-api -p 9080:8080 \
   -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e SCHEMA_REGISTRY_HOST=schema-registry -e EUREKA_HOST=eureka -e ZIPKIN_HOST=zipkin \
   --network=spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
   ivanfranchin/producer-api:1.0.0
 
 wait_for_container_log "producer-api" "Started"
@@ -29,7 +29,7 @@ echo "Starting categorizer-service..."
 docker run -d --rm --name categorizer-service -p 9081:8080 \
   -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e SCHEMA_REGISTRY_HOST=schema-registry -e EUREKA_HOST=eureka -e ZIPKIN_HOST=zipkin \
   --network=spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
   ivanfranchin/categorizer-service:1.0.0
 
 wait_for_container_log "categorizer-service" "Started"
@@ -40,7 +40,7 @@ echo "Starting collector-service..."
 docker run -d --rm --name collector-service -p 9082:8080 \
   -e ELASTICSEARCH_HOST=elasticsearch -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e SCHEMA_REGISTRY_HOST=schema-registry -e EUREKA_HOST=eureka -e ZIPKIN_HOST=zipkin \
   --network=spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
   ivanfranchin/collector-service:1.0.0
 
 wait_for_container_log "collector-service" "Started"
@@ -51,7 +51,7 @@ echo "Starting publisher-api..."
 docker run -d --rm --name publisher-api -p 9083:8080 \
   -e ELASTICSEARCH_HOST=elasticsearch -e EUREKA_HOST=eureka -e ZIPKIN_HOST=zipkin \
   --network=spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
   ivanfranchin/publisher-api:1.0.0
 
 wait_for_container_log "publisher-api" "Started"
@@ -62,7 +62,7 @@ echo "Starting news-client..."
 docker run -d --rm --name news-client -p 8080:8080 \
   -e KAFKA_HOST=kafka -e KAFKA_PORT=9092 -e SCHEMA_REGISTRY_HOST=schema-registry -e EUREKA_HOST=eureka -e ZIPKIN_HOST=zipkin \
   --network=spring-cloud-stream-kafka-elasticsearch_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" --health-start-period=1m \
+  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
   ivanfranchin/news-client:1.0.0
 
 wait_for_container_log "news-client" "Started"
