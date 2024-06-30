b26a0df212b2609f921a4fba3fec1a731ffc3fb4
Ivan Franchin <ivan.franchin@takeaway.com>
2024-06-30 09:52:43 +0200

    Project upgrade
    - upgrade to spring-boot 3.3.1;
    - upgrade to spring-cloud-stream 2023.0.2;
    - upgrade to jib-maven-plugin 3.4.3;
    - upgrade to amazoncorretto-jdk 17.0.11;
    - upgrade wrapperVersion to 3.3.2 and apache-maven to 3.9.7;
    - upgrade to springdoc-openapi 2.5.0;
    - upgrade to elasticsearch docker image 8.13.4;
    - upgrade to confluentinc docker images 7.6.1;
    - upgrade to jquery 3.7.1;
    - upgrade to moment.js 2.30.1;
    - remove version from docker-compose.yml;
    - fix the output-bindings property by news-out-0 instead of news;
    - removed exclusion of org.apache.httpcomponents:httpclient in spring-cloud-starter-netflix-eureka-client present in collector and publisher;
    - add SpringDataWebSupportConfig class, see https://docs.spring.io/spring-data/jpa/reference/repositories/core-extensions.html\#core.web.page.config
    - update README.

---
+++ publisher-api/src/main/java/com/ivanfranchin/publisherapi/config/SpringDataWebSupportConfig.java
@@ -0,0 +1,11 @@
+package com.ivanfranchin.publisherapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class SpringDataWebSupportConfig {
+}
