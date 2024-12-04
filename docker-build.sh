fbc6a63b7a72a67c9eb385ef8f2f1e24f00987de
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-04 11:54:28 -0300

    Project upgrade
    - upgrade to spring-boot 3.4.0;
    - upgrade to spring-could 2024.0.0;
    - upgrade to springdoc-openapi 2.7.0;
    - upgrade to mapstruct 1.6.3;
    - upgrade to confluentinc docker images 7.8.0;
    - upgrade to elasticsearch docker image 8.15.4;
    - upgrade to zipkin docker image 3.4.1;
    - remove logging.pattern.level as it was not needed;
    - refactor docker-compose.yml, docker-build.sh, and pom.xml files;
    - update README.

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,50 @@
 #!/usr/bin/env bash
 
-./mvnw clean compile jib:dockerBuild --projects eureka-server
-./mvnw clean compile jib:dockerBuild --projects producer-api
-./mvnw clean compile jib:dockerBuild --projects categorizer-service
-./mvnw clean compile jib:dockerBuild --projects collector-service
-./mvnw clean compile jib:dockerBuild --projects publisher-api
-./mvnw clean compile jib:dockerBuild --projects news-client
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_VERSION="1.0.0"
+
+EUREKA_SERVER_APP_NAME="eureka-server"
+PRODUCER_API_APP_NAME="producer-api"
+CATEGORIZER_SERVICE_APP_NAME="categorizer-service"
+COLLECTOR_SERVICE_APP_NAME="collector-service"
+PUBLISHER_API_APP_NAME="publisher-api"
+NEWS_CLIENT_APP_NAME="news-client"
+
+EUREKA_SERVER_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${EUREKA_SERVER_APP_NAME}:${APP_VERSION}"
+PRODUCER_API_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${PRODUCER_API_APP_NAME}:${APP_VERSION}"
+CATEGORIZER_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${CATEGORIZER_SERVICE_APP_NAME}:${APP_VERSION}"
+COLLECTOR_SERVICE_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${COLLECTOR_SERVICE_APP_NAME}:${APP_VERSION}"
+PUBLISHER_API_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${PUBLISHER_API_APP_NAME}:${APP_VERSION}"
+NEWS_CLIENT_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${NEWS_CLIENT_APP_NAME}:${APP_VERSION}"
+
+SKIP_TESTS="true"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$EUREKA_SERVER_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$EUREKA_SERVER_DOCKER_IMAGE_NAME"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$PRODUCER_API_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$PRODUCER_API_DOCKER_IMAGE_NAME"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$CATEGORIZER_SERVICE_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$CATEGORIZER_SERVICE_DOCKER_IMAGE_NAME"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$COLLECTOR_SERVICE_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$COLLECTOR_SERVICE_DOCKER_IMAGE_NAME"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$PUBLISHER_API_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$PUBLISHER_API_DOCKER_IMAGE_NAME"
+
+./mvnw clean compile jib:dockerBuild \
+  --projects "$NEWS_CLIENT_APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$NEWS_CLIENT_DOCKER_IMAGE_NAME"
